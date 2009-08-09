574b911948e7b434260e9c3c0e2cc70cf34dac97
Gökmen Görgen <gkmngrgn@gmail.com>
2009-08-10 02:46:39 +0300

    change directory hierarchy and update setup.py

--- setup.py
+++ setup.py
@@ -10,45 +10,54 @@ import os
 import shutil
 
 from distutils.core import setup
-from src.common import (app_launch_name, app_version, app_description, \
-                        core_developer, core_developer_email, app_url, \
-                        app_license_name)
 
-script = '%s/%s' % (app_launch_name, app_launch_name)
-shutil.copyfile('%s.py' % app_launch_name, script)
+shutil.copytree("src/", "puic/")
+from puic.common import (app_launch_name, app_version, app_description, \
+                         core_developer, core_developer_email, app_url, \
+                         app_license_name)
+
+script = "%s/%s" % (app_launch_name, app_launch_name)
+shutil.copyfile("%s/%s.py" % (app_launch_name, app_launch_name), script)
 os.chmod(script, 0755)
 
-DOC_DIR = '/usr/share/doc'
-SBIN_DIR = '/usr/sbin'
-LOCALE_DIR = '/usr/share/locale'
-LANGS = ['tr', ]
+LANGS = ["tr"]
 
+# General installation functions
 def locale(lang):
-    return('share/locale/%s/LC_MESSAGES' % lang,
-            ['po/locale/%s/%s.mo' % (lang, app_launch_name)])
+    return("share/locale/%s/LC_MESSAGES" % lang,
+            ["datas/po/locale/%s/%s.mo" % (lang, app_launch_name)])
 
-#Create .mo files
-if not os.path.exists('po/locale'):
-    os.mkdir('po/locale')
+def removeBuildFiles():
+    rmDir = ["build", "datas/po/locale", app_launch_name]
+
+    # remove build directories
+    for dir in rmDir:
+        print("Removing directory, %s.." % dir)
+        shutil.rmtree(dir)
+
+    # remove compiled Python files.
+    for file in os.listdir("./"):
+        if file.endswith(".pyc"):
+            os.remove(file)
+
+# Create .mo files
+if not os.path.exists("datas/po/locale"):
+    os.mkdir("datas/po/locale")
 
     for lang in LANGS:
-        pofile = 'po/' + lang + '.po'
-        mofile = 'po/locale/' + lang + '/%s.mo' % app_launch_name
+        pofile = "datas/po/" + lang + ".po"
+        mofile = "datas/po/locale/" + lang + "/%s.mo" % app_launch_name
 
-        os.mkdir('po/locale/' + lang + '/')
+        os.mkdir("datas/po/locale/" + lang + "/")
         print("generating %s" % mofile)
-        os.system('msgfmt %s -o %s' % (pofile, mofile))
+        os.system("msgfmt %s -o %s" % (pofile, mofile))
 
 datas = [
-    ('%s/%s' % (DOC_DIR, app_launch_name), ['ChangeLog', \
-                                            'COPYING', \
-                                            'NOTES', \
-                                            'README', \
-                                            'TODO']),
-    (SBIN_DIR, [script, ]),
-    ('share/%s' % app_launch_name, glob.glob('datas/syslinux.cfg.*')),
-    locale('tr'),
-    ]
+    ("share/doc/%s" % (DOC_DIR, app_launch_name), ["ChangeLog", "COPYING", "NOTES", \
+                                            "README", "TODO"]),
+    ("share/%s" % app_launch_name, glob.glob("datas/syslinux.cfg.*")),
+#    ("share/%s/ui" % app_launch_name, glob.glob("datas/ui/*")),
+    locale("tr")]
 
 setup(
     name = app_launch_name,
@@ -58,8 +67,10 @@ setup(
     author_email = core_developer_email,
     url = app_url,
     license = app_license_name,
-    platforms = ['Linux', ],
-    packages = ['%s/' % app_launch_name, ],
+    packages = [app_launch_name],
+    scripts = [script],
     data_files = datas,
     )
 
+# Clean build files and directories
+removeBuildFiles()
