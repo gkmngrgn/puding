c65ef83abed3653a056aaacb2cb2ef4ad5c00afd
Gökmen Görgen <gkmngrgn@gmail.com>
2009-08-13 18:12:30 +0300

    add OK button.

--- src/ui_qt.py
+++ src/ui_qt.py
@@ -4,24 +4,26 @@
 # author: Gökmen Görgen
 # license: GPLv3
 
+import os
 import sys
-from os import path
 
-from common import share_dir
+from common import (NAME, getDiskInfo)
 from PyQt4 import (QtCore, QtGui, uic)
 
 class Create(QtGui.QMainWindow):
     def __init__(self, parent = None):
         super(Create, self).__init__(parent)
-        uic.loadUi("%s/ui/qtMain.ui" % share_dir, self)
+        uic.loadUi("%s/ui/qtMain.ui" % NAME, self)
         self.text_src = self.label_src.text()
         self.text_dst = self.label_dst.text()
 
         self.connect(self.button_next, QtCore.SIGNAL("clicked()"), self.__actionNext)
         self.connect(self.button_prev, QtCore.SIGNAL("clicked()"), self.__actionPreview)
+        self.connect(self.button_ok, QtCore.SIGNAL("clicked()"), self.__actionOk)
         self.connect(self.button_quit, QtCore.SIGNAL("clicked()"), QtCore.SLOT("close()"))
 
         self.button_prev.hide()
+        self.button_ok.hide()
 
     def __actionNext(self):
         src = self.line_src.displayText()
@@ -29,33 +31,43 @@ class Create(QtGui.QMainWindow):
 
         if not self.__checkSource(src):
             self.label_warning.setText("<font color=\"red\">The ISO path you have specified is invalid!</font>")
-            
+
             return False
 
         if not self.__checkDestination(dst):
             self.label_warning.setText("<font color=\"red\">The USB disk path you have specified is invalid!</font>")
-            
+
             return False
 
         self.label_warning.setText("<i>The paths you have specified are valid..</i>")
 
         id = self.stackedWidget.currentIndex()
-        if id < 1:
-            self.stackedWidget.setCurrentIndex(id + 1)
 
         if id == 0:
+            self.button_next.hide()
+            self.button_ok.show()
             self.button_prev.show()
-            
+
+            self.__checkInformation(src, dst)
+            self.stackedWidget.setCurrentIndex(id + 1)
+
         return True
 
     def __actionPreview(self):
         id = self.stackedWidget.currentIndex()
-        
+
         if id > 0:
             self.stackedWidget.setCurrentIndex(id - 1)
 
         if id == 1:
             self.button_prev.hide()
+            self.button_ok.hide()
+            self.button_next.show()
+
+    def __actionOk(self):
+        id = self.stackedWidget.currentIndex()
+
+        self.stackedWidget.setCurrentIndex(id + 1)
 
     def __checkSource(self, src):
         if QtCore.QString(src).isEmpty():
@@ -63,13 +75,21 @@ class Create(QtGui.QMainWindow):
             return False
 
         else:
-            src_extension = path.splitext(str(src))[1] == ".iso"
+            src_extension = os.path.splitext(str(src))[1] == ".iso"
 
-            return path.isfile(src) and src_extension
+            return os.path.isfile(src) and src_extension
 
     def __checkDestination(self, dst):
         if QtCore.QString(dst).isEmpty():
             return False
 
         else:
-            return path.ismount(str(dst))
+            return os.path.ismount(str(dst))
+
+    def __checkInformation(self, src, dst):
+        (capacity, available, used) = getDiskInfo(str(dst))
+
+        self.label_info_source.setText(src)
+        self.label_info_capacity.setText(str(capacity))
+        self.label_info_available.setText(str(available))
+        self.label_info_used.setText(str(used))
