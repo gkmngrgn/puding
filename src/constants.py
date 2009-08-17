78168d7421403a29224a667cfa4a64e361d8cca8
Gökmen Görgen <gkmngrgn@gmail.com>
2009-08-18 02:18:53 +0300

    add missing funcs, add SUMMARY and USAGE variables.

--- src/constants.py
+++ src/constants.py
@@ -4,22 +4,24 @@
 # author: Gökmen Görgen
 # license: GPLv3
 
+from os import getenv
+
 NAME = "puic"
 LOCALE = "/usr/share/locale"
 VERSION = "0.1"
-HOME = "%s/.puic" % os.getenv("HOME")
+HOME = "%s/.puic" % getenv("HOME")
 SHARE = "/usr/share/%s" % NAME
 SYSLINUX = "/usr/lib/syslinux"
 GFXTHEME = "/usr/share/gfxtheme/"
-DESCRIPTION = _("An USB Image Creator For Pardus Linux.")
 URL = "http://www.gokmengorgen.net/puic"
 LICENSE_NAME = "GPLv3"
 CORE_DEVELOPER = "Gökmen Görgen"
 CORE_EMAIL = "gkmngrgn@gmail.com"
-COPYRIGHT = _("Copyright") + " \302\251 2009 Gökmen Görgen, <%s>" % CORE_EMAIL
-LICENSE = _("""\
-Puic is an USB image creator for Pardus Linux.
-%s
+SUMMARY = "An USB Image Creator For Pardus Linux."
+DESCRIPTION = "Puic is an USB image creator for Pardus Linux."
+COPYRIGHT = "Copyright \302\251 2009 Gökmen Görgen, <%s>" % CORE_EMAIL
+USAGE = "%s\n%s\n\nUsage: %s [options]" % (DESCRIPTION, COPYRIGHT, NAME)
+LICENSE = """%s
 
 Puic is a free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -33,4 +35,4 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.\
-""" % COPYRIGHT)
+""" % COPYRIGHT
